7dae097f10c5448bb44d0834bc9ea87ff789e2d7
joe733 <jovial7joe@hotmail.com>
2020-10-01 14:57:28 +0530

    fix: if-else skips 'else' if 'if' is true
    Takes all the times t1..t3 and then compared them instead of skiping over.
    Also removed unused variables.

--- Embedded-Digispark/ATTiny85_Slide-O/src/main.cpp
+++ Embedded-Digispark/ATTiny85_Slide-O/src/main.cpp
@@ -1,43 +1,41 @@
 #include <Arduino.h>
- #include <DigiMouse.h>
+#include <DigiMouse.h>
 
 #define cSens1 0
 #define cSens2 1
 #define cSens3 2
 
-unsigned long t1;
-unsigned long t2;
-unsigned long t3;
- 
-bool upscroll;
-bool dscroll;
+unsigned long t1, t2, t3;
 
-void setup() {
-  DigiMouse.begin(); //start or reenumerate USB - BREAKING CHANGE from old versions that didn't require this
+void setup()
+{
+  DigiMouse.begin(); // start or reenumerate USB - BREAKING CHANGE from old versions that didn't require this
+  t1 = t1 = t3 = 0;
   pinMode(cSens1, INPUT);
   pinMode(cSens2, INPUT);
   pinMode(cSens3, INPUT);
 }
+void loop()
+{
+  // Creating Timestamp
+  if (digitalRead(cSens1))
+    t1 = millis();
+  if (digitalRead(cSens2))
+    t2 = millis();
+  if (digitalRead(cSens3))
+    t3 = millis();
 
-void loop() {
- if (digitalRead(cSens1))             //Creating Timestamp
-   t1=millis();
- else if (digitalRead(cSens2))
-   t2=millis();
- else if (digitalRead(cSens3)){
-   t3=millis();
- }
- 
- else if(t3>t1 && t1!=0){                    //Comparing Timestamp
-   DigiMouse.scroll(2);
-   t1=t2=t3=0;                      // Once executed Resetting all timestamps
- }
- else if(t3<t1 && t3!=0){
-   DigiMouse.scroll(-2);
-   t1=t2=t3=0;                      // Once executed Resetting all timestamps
- }
- else
-   DigiMouse.update();
-
-//  DigiMouse.delay(500);
+  // Comparing Timestamps
+  if (t3 > t1 && t1 != 0)
+  {
+    DigiMouse.scroll(2);
+  }
+  else if (t3 < t1 && t3 != 0)
+  {
+    DigiMouse.scroll(-2);
+  }
+  else
+    DigiMouse.update();
+  t1 = t2 = t3 = 0; // Once executed reset all timestamps
+                    // DigiMouse.delay(500);
 }
